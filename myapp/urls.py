5cdcffedd7036cb741f38302ffd15126a882c72b
Amit Shalev <amit30005@gmail.com>
2020-06-07 20:18:44 +0300

    fix

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='write'),
+    path('', views.index, name='index'),
     path('write/', views.write_message, name='write'),
     path('get_all/<str:receiver>', views.get_all_messages_for_receiver, name='get_all_reciver'),
     path('get_unread/<str:receiver>', views.get_unread_messages_for_receiver, name='get_unread_reciver'),
